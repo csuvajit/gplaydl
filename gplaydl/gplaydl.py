b87984740507d8b2c2a916ae195722972e8ad784
Suvajit <suvajit.me@gmail.com>
2021-11-05 19:42:27 +0530

    Update gplaydl.py

--- gplaydl/gplaydl.py
+++ gplaydl/gplaydl.py
@@ -56,7 +56,8 @@ def sizeof_fmt(num):
 
 
 def configureauth():
-    print(os.environ["GOOGLE_EMAIL"])
+    printf("---------------------------------")
+    printf(os.environ["GOOGLE_EMAIL"])
     email = os.environ.get('GOOGLE_EMAIL')
     password = os.environ.get('GOOGLE_PASSWORD')
     while email is None:
